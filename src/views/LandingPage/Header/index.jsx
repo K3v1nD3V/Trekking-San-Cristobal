cad3733d47a0460b3522df2d2ea1f27be34e351f
Kevin Correa T <kctDEV494@gmail.com>
2024-08-03 09:26:30 -0500

    Correccion de errores, recolocacion del importe de las fuentes, entre otros cambios minimos

---
+++ src/views/LandingPage/Header/index.jsx
@@ -0,0 +1,27 @@
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div>
+                    <div>
+                        <a href="/">Logo</a>
+                    </div>
+                    <nav>
+                        <i className="fa-solid fa-bars"></i>
+                    </nav>
+                </div>
+                <div>
+                    <div>
+                        <h1>Header</h1>
+                    </div>
+                    <div>
+                        <button>Accion</button>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export { Header };
