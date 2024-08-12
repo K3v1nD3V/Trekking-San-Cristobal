d92f041b232eb1031ccfd05c0b23ad6286991fff
Kevin Correa T <kctDEV494@gmail.com>
2024-08-12 07:41:51 -0500

    Añado la hoja de estilo del header

---
+++ src/views/LandingPage/Header/components/menu.jsx
@@ -0,0 +1,24 @@
+function Menu() {
+    return (
+        <>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Sobre Nosotros</a>
+                        </li>
+                        <li>
+                            <a href="#">Recorridos</a>
+                        </li>
+                        <li>
+                            <a href="#">Favoritos</a>
+                        </li>
+                        <li>
+                            <a href="#">Servios</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
